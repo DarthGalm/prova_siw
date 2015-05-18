b2bbebca95effc7e46ff9edb689a27f6a0bc873c
DarthGalm <filippo.frillici@gmail.com>
2015-05-18 22:13:12 +0200

    prova commit

---
+++ src/it/uniroma3/servlet/Prova.java
@@ -0,0 +1,20 @@
+package it.uniroma3.servlet;
+
+public class Prova {
+	
+	String prova;
+	
+	public Prova(){
+		
+	}
+	
+	public String getProva(){
+		return this.prova;
+	}
+
+	public void setProva(String prova) {
+		this.prova = prova;
+	}
+	
+
+}
